234a2c1990220503669146ed12ce0413bb6e878d
cesar-filho <cesar.pc.filho@gmail.com>
2020-02-15 18:57:06 -0300

    implementado envio de email ao finalizar prova

--- src/api/controllers/api.js
+++ src/api/controllers/api.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import nodemailer from 'nodemailer'
 import { OK, CREATED } from 'http-status-codes'
 
 import provas from '../objects/provas'
@@ -9,7 +10,7 @@ const router = express.Router()
 
 const TableProvas = process.env.TABLE_PROVAS
 const TableQuestoes = process.env.TABLE_QUESTOES
-const TableRespostas = process.env.TABLE_REPOSTAS
+const TableRespostas = process.env.TABLE_RESPOSTAS
 
 router.get('/disciplina', async (req, res) => {
   res.status(OK).send({ disciplina: provas })
@@ -29,7 +30,7 @@ router.get('/provas/:prova_id/questoes', async (req, res) => {
   return res.status(OK).json(questoes)
 })
 
-router.get('/provas/:prova_id/questao/:questao_id', async (req, res) => {
+router.get('/provas/:prova_id/questoes/:questao_id', async (req, res) => {
   const { questao_id } = req.params
 
   const response = await dc.scan({ TableName: TableQuestoes })
@@ -52,6 +53,48 @@ router.get('/provas/:prova_id/respostas', async (req, res) => {
   const respostas = response.Items.filter(i => prova_id == i.prova_id)
 
   res.status(CREATED).send(respostas)
+})
+
+router.post('/provas/:prova_id/finalizar', async (req, res) => {
+  const { prova_id } = req.params
+
+  const resQuestoes = await dc.scan({ TableName: TableQuestoes })
+  const resRespostas = await dc.scan({ TableName: TableRespostas })
+
+  const questoes = resQuestoes.Items.filter(i => i.prova_id == prova_id).sort((a, b) => a.numero - b.numero)
+  const respostas = resRespostas.Items.filter(i => i.prova_id == prova_id)
+
+  let email = ''
+  for (const questao of questoes) {
+    email += `${questao.numero}º questão: ${questao.descricao} \n`
+    const resposta = respostas.find(r => r.questao_id == questao.id)
+
+    if (resposta) {
+      email += `Resposta: ${resposta.valor} \n\n`
+    } else {
+      email += `Não respondido! \n\n`
+    }
+  }
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    auth: {
+      user: '2aa30e92e36789',
+      pass: '2f1d6e5ad01866',
+    },
+  })
+
+  const mailOptions = {
+    from: 'grupohoradaprova@gmail.com',
+    to: 'grupohoradaprova@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: email,
+  }
+
+  await transporter.sendMail(mailOptions)
+
+  res.send({ message: 'Enviado com sucesso!' })
 })
 
 export default router
