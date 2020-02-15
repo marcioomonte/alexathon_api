909f18528098db098d4be0b145e8600d4bd0c7e2
cesar-filho <cesar.pc.filho@gmail.com>
2020-02-15 16:41:43 -0300

    Implementado post e get de respostas

--- src/api/controllers/api.js
+++ src/api/controllers/api.js
@@ -4,12 +4,14 @@ import { OK } from 'http-status-codes'
 import provas from '../objects/provas'
 import questoes from '../objects/questoes'
 import DynamoClient from '../utils/dynamo-client'
+import { CREATED } from '../../../.webpack/service/src/api/bin'
 
 const dc = new DynamoClient()
 const router = express.Router()
 
 const TableProvas = process.env.TABLE_PROVAS
 const TableQuestoes = process.env.TABLE_QUESTOES
+const TableRespostas = process.env.TABLE_REPOSTAS
 
 router.get('/disciplina', async (req, res) => {
   res.status(OK).send({ disciplina: provas })
@@ -36,6 +38,22 @@ router.get('/provas/:prova_id/questao/:questao_id', async (req, res) => {
   const questao = response.Items.find(i => questao_id == i.id)
 
   res.status(OK).json(questao)
+})
+
+router.post('/provas/:prova_id/respostas', async (req, res) => {
+  const resposta = req.body
+
+  await dc.put({ TableName: TableRespostas, Item: resposta })
+
+  res.status(CREATED).send()
+})
+
+router.get('/provas/:prova_id/respostas', async (req, res) => {
+  const { prova_id } = req.params
+  const response = await dc.scan({ TableName: TableRespostas })
+  const respostas = response.Items.filter(i => prova_id == i.prova_id)
+
+  res.status(CREATED).send(respostas)
 })
 
 export default router
