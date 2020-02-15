5931f3a07e62dacdaf2f66e4172a8984d014a1f9
Marcio <marcioomonte@gmail.com>
2020-02-15 15:47:13 -0300

    objetos de provas e questoes

--- src/api/controllers/api.js
+++ src/api/controllers/api.js
@@ -2,22 +2,27 @@ import express from 'express';
 import { OK } from 'http-status-codes';
 
 import provas from '../objects/provas';
+import questoes from '../objects/questoes';
 const router = express.Router();
 
 router.get('/disciplina', async (req, res) => {
-  res.status(OK).send({ materia: 'historia' });
+  res.status(OK).send({ disciplina: provas });
 });
 
 router.get('/provas', async (req, res) => {
   res.status(OK).send({ provas: provas });
 });
 
-router.get('/provas/:id/questao', async (req, res) => {
-  res.status(OK).send({ questao: `${req.params.id}` });
+router.get('/provas/:prova_id/questoes', async (req, res) => {
+  const { prova_id } = req.params;
+
+  const response = questoes.filter(q => q.prova_id === prova_id);
+
+  return res.status(OK).json(response);
 });
 
 router.post('/prova/:id/questao', async (req, res) => {
-  res.status(OK).send({ questao: `${req.params.id}` });
+  res.status(OK).json({ questao: `${req.params.id}` });
 });
 
 export default router;
---
+++ src/api/objects/questoes.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    id: 1111,
+    prova_id: 1,
+    numero: 1,
+    descricao:
+      'As capitanias hereditárias foram criadas com a intenção de dar a monarquia portuguesa controle sobre as regiões do país, como foram organizadas essas capitania?',
+    dica:
+      'ainda usamos essa divisão no Brasil para dar controle aos estados do país',
+
+    respondida: false
+  },
+  {
+    id: 1112,
+    prova_id: 1,
+    numero: 2,
+    descricao: 'essa é a questão 2',
+    dica: 'dica da questao 2',
+    respondida: false
+  },
+  ,
+  {
+    id: 1113,
+    prova_id: 1,
+    numero: 3,
+    descricao: 'essa é a questão 3',
+    dica: 'dica da questao 3',
+    respondida: false
+  },
+  {
+    id: 1114,
+    prova_id: 1,
+    numero: 4,
+    descricao: 'essa é a questão 4',
+    dica: 'dica da questao 4',
+    respondida: false
+  },
+  {
+    id: 1115,
+    prova_id: 1,
+    numero: 5,
+    descricao: 'essa é a questão 5',
+    dica: 'dica da questao 5',
+    respondida: false
+  }
+];
